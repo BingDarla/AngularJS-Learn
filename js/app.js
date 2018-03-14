7117c0794e652a030076d27e2c7cb8efed94b1c9
Bingshuang Han <bingshuang@BingHans-MacBook-Pro.local>
2018-03-14 16:06:17 +1100

    weather forecast

--- js/app.js
+++ js/app.js
@@ -2,20 +2,33 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 //Routes
-weatherApp.config(function($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('');
-    $routeProvider
-        .when('/', {
-            templateUrl: '/js/pages/home.html',
+weatherApp.config(function($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $routeProvider
+            .when('/', {
+                templateUrl: 'js/pages/home.html',
+                controlelr: 'homeController'
 
-        })
+            })
+            .when('/forecast', {
+                templateUrl: 'js/pages/weather.html',
+                controller: 'forecastController'
+            })
+    })
+    //service
+weatherApp.service("cityService", function() {
+    this.city = "New York, NY";
 })
 
 //Controller
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    $scope.city = cityService.city;
 
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    })
 }]);
 //Controller
-weatherApp.controller('forecastController', ['$scope', function($scope) {
-
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+    $scope.city = cityService.city;
 }]);
